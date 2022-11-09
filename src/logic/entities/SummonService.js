0a2e412fb9ffb1a7e7ae7a2da15e65846a12c098
Bissbert <43237892+Bissbert@users.noreply.github.com>
2022-11-09 15:16:50 +0100

    rules for falling items are set and refactor from bread to item happened

--- src/logic/entities/SummonService.js
+++ src/logic/entities/SummonService.js
@@ -1,7 +1,8 @@
 import { getGameScreenWidth } from '../gameScreenSize';
-import FallingBread, { FALLING_RADIUS } from '../../components/entities/FallingBread';
+import FallingItem, { FALLING_RADIUS } from '../../components/entities/FallingItem';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
+import { ItemEntity, itemTypes } from './ItemEntity';
 
 let nextSummonTime: number;
 
@@ -9,26 +10,58 @@ let nextSummonTime: number;
 const TIME_MIN = 1000;
 const TIME_MAX = 4000;
 
+/**
+ * service to summon items
+ * @param entities the list of entities currently in the game
+ * @returns the updated list of entities
+ */
 const SummonService = entities => {
   //if no summon time or time is reached, summon a new enemy
   if (!nextSummonTime || nextSummonTime < new Date().getTime()) {
     const id = uuidv4();
-    //random position from 0 + FALLING_RADIUS to the screen width - FALLING_RADIUS
-    const x =
-      Math.floor(Math.random() * (getGameScreenWidth() - FALLING_RADIUS * 2)) +
-      FALLING_RADIUS;
-    const y = 100;
-    const position = [x, y];
-    entities[id] = {
-      position: position,
-      radius: FALLING_RADIUS,
-      renderer: <FallingBread />,
-    };
+    entities[id] = generateEntity();
+    console.log('summoned item', entities[id]);
     resetSummonTime();
   }
   return entities;
 };
 
+/**
+ * function that returns a random item type
+ * the chances are 80% for bread and 20% for fork
+ * @returns {ItemType}
+ */
+function randomItemType() {
+  const random = Math.random();
+  if (random < 0.8) {
+    return itemTypes.bread;
+  } else {
+    return itemTypes.fork;
+  }
+}
+
+/**
+ * function that generates a new entity
+ * @returns {ItemEntity}
+ */
+const generateEntity = () => {
+  //random position from 0 + FALLING_RADIUS to the screen width - FALLING_RADIUS
+  const x =
+    Math.floor(Math.random() * (getGameScreenWidth() - FALLING_RADIUS * 2)) +
+    FALLING_RADIUS;
+  const y = 100;
+  const position = [x, y];
+  return new ItemEntity(
+    position,
+    <FallingItem />,
+    FALLING_RADIUS,
+    randomItemType(),
+  );
+};
+
+/**
+ * function that resets the summon time
+ */
 const resetSummonTime = () => {
   nextSummonTime =
     Date.now() + Math.random() * (TIME_MAX - TIME_MIN) + TIME_MIN;
